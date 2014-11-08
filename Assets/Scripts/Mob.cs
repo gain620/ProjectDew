e97f4a40308f76c6f3d8431a753525b63bfb52ed
Gain Chang <gain620@naver.com>
2014-11-09 03:24:31 +0900

    added some very basic combat system and mob ai :D

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour 
+{
+
+	public float speed;
+	public float range; // add attack range?
+
+	public CharacterController controller;
+	public Transform player;
+
+	public AnimationClip idle,run;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log (inRange ());
+
+		if(!inRange())
+		{
+			chase ();
+		}
+		else
+		{
+			animation.CrossFade(idle.name);
+		}
+		
+	}
+
+	bool inRange()
+	{
+		if (Vector3.Distance (transform.position, player.position) < range) 
+		{
+			return true;
+		} 
+		else
+		{
+			return false;
+		}
+
+		// change to the line below for optimization later!~
+		//return Vector3.Distance (transform.position, player.position) < range;
+	}
+
+	void chase()
+	{
+		// hmm.. more smooth transition???
+		transform.LookAt(player.position);
+		controller.SimpleMove(transform.forward*speed);
+		animation.CrossFade(run.name);
+
+	}
+
+	void OnMouseOver()
+	{
+		player.GetComponent<Combat>().opponent = gameObject ;
+	}
+}
